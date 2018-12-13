f34fe569b9ad712a6137ec5221baa1706cd6441d
Josué Furlan Machado <josue.furlan@peixeurbano.com>
2018-12-12 23:43:55 -0200

    final first commit

--- jace/page_files/save_files.py
+++ jace/page_files/save_files.py
@@ -1,4 +1,4 @@
 def save_file(file_name, card_name, content):
-    with open(f"../../jace/page_files/{file_name}/{card_name}.html", "wb") as f:
+    with open(f"../../jace/page_files/spider_pages/{file_name}/{card_name}.html", "wb") as f:
         f.write(content)
 
